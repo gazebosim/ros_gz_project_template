f76b85ed4e79e02ac9106886f3860dca67042483
Dharini Dutia <dharinidutia@gmail.com>
2022-10-12 14:36:31 -0700

    Example ros_gz package in support of presentation (#1)
    * Skeleton
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    * Add package.xml
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    * restructure and adding required files
    
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>
    
    * reverted package names
    
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>
    
    * hooks, cmakelists and ci
    
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>
    
    * remove submodule, added yaml, renaming and updates
    
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>
    
    * Move packages to root (#2)
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    * Moving around a bit and adding yaml bridge config
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>
    Co-authored-by: Michael Carroll <michael@openrobotics.org>

---
+++ ros_gz_example_bringup/launch/diff_drive.launch.py
@@ -0,0 +1,71 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_project_bringup = get_package_share_directory('ros_gz_example_bringup')
+    pkg_project_description = get_package_share_directory('ros_gz_example_description')
+
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={'gz_args': PathJoinSubstitution([
+            pkg_project_description,
+            'worlds',
+            'diff_drive.sdf'
+        ])}.items(),
+    )
+
+    # RViz
+    rviz = Node(
+       package='rviz2',
+       executable='rviz2',
+       arguments=['-d', os.path.join(pkg_project_bringup, 'rviz', 'ros_gz_example.rviz')],
+       condition=IfCondition(LaunchConfiguration('rviz'))
+    )
+
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{
+            'config_file': os.path.join(pkg_project_bringup, 'config', 'ros_gz_example_bridge.yaml'),
+            'qos_overrides./model/vehicle_blue.subscriber.reliability': 'reliable',
+            'qos_overrides./model/vehicle_green.subscriber.reliability': 'reliable'
+        }],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gz_sim,
+        DeclareLaunchArgument('rviz', default_value='true',
+                              description='Open RViz.'),
+        bridge,
+        rviz
+    ])
