ffcd56186308630c0afd69900800b4f7507937d7
Dharini Dutia <dharini@openrobotics.org>
2023-11-30 15:19:47 -0800

    Making Fortress compatible (#13)
    * deps and build instructions
    
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>
    
    * namespaces
    
    * env variables
    
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>
    
    * dependencies
    
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>
    
    ---------
    
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>

--- ros_gz_example_gazebo/src/FullSystem.cc
+++ ros_gz_example_gazebo/src/FullSystem.cc
@@ -29,14 +29,13 @@
 
 // This is required to register the plugin. Make sure the interfaces match
 // what's in the header.
-GZ_ADD_PLUGIN(
+IGNITION_ADD_PLUGIN(
     ros_gz_example_gazebo::FullSystem,
     gz::sim::System,
     ros_gz_example_gazebo::FullSystem::ISystemConfigure,
     ros_gz_example_gazebo::FullSystem::ISystemPreUpdate,
     ros_gz_example_gazebo::FullSystem::ISystemUpdate,
-    ros_gz_example_gazebo::FullSystem::ISystemPostUpdate,
-    ros_gz_example_gazebo::FullSystem::ISystemReset
+    ros_gz_example_gazebo::FullSystem::ISystemPostUpdate
 )
 
 namespace ros_gz_example_gazebo 
@@ -47,7 +46,7 @@ void FullSystem::Configure(const gz::sim::Entity &_entity,
                 gz::sim::EntityComponentManager &_ecm,
                 gz::sim::EventManager &_eventManager)
 {
-  gzdbg << "ros_gz_example_gazebo::FullSystem::Configure on entity: " << _entity << std::endl;
+  igndbg << "ros_gz_example_gazebo::FullSystem::Configure on entity: " << _entity << std::endl;
 }
 
 void FullSystem::PreUpdate(const gz::sim::UpdateInfo &_info,
@@ -55,7 +54,7 @@ void FullSystem::PreUpdate(const gz::sim::UpdateInfo &_info,
 {
   if (!_info.paused && _info.iterations % 1000 == 0)
   {
-    gzdbg << "ros_gz_example_gazebo::FullSystem::PreUpdate" << std::endl;
+    igndbg << "ros_gz_example_gazebo::FullSystem::PreUpdate" << std::endl;
   }
 }
 
@@ -64,7 +63,7 @@ void FullSystem::Update(const gz::sim::UpdateInfo &_info,
 {
   if (!_info.paused && _info.iterations % 1000 == 0)
   {
-    gzdbg << "ros_gz_example_gazebo::FullSystem::Update" << std::endl;
+    igndbg << "ros_gz_example_gazebo::FullSystem::Update" << std::endl;
   }
 }
 
@@ -73,13 +72,8 @@ void FullSystem::PostUpdate(const gz::sim::UpdateInfo &_info,
 {
   if (!_info.paused && _info.iterations % 1000 == 0)
   {
-    gzdbg << "ros_gz_example_gazebo::FullSystem::PostUpdate" << std::endl;
+    igndbg << "ros_gz_example_gazebo::FullSystem::PostUpdate" << std::endl;
   }
 }
 
-void FullSystem::Reset(const gz::sim::UpdateInfo &_info,
-                       gz::sim::EntityComponentManager &_ecm)
-{
-  gzdbg << "ros_gz_example_gazebo::FullSystem::Reset" << std::endl;
-}
-}  // namespace ros_gz_example_gazeba
+}  // namespace ros_gz_example_gazebo
